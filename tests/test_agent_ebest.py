4a49a4ad5913f6d9006054db7800ac5d79dc4581
mr-newbe <49405838+mr-newbe@users.noreply.github.com>
2024-02-18 19:18:00 +0900

    Update test_agent_ebest.py-- add function test_code_list

--- tests/test_agent_ebest.py
+++ tests/test_agent_ebest.py
@@ -12,3 +12,20 @@ class TestEBest(unittest.TestCase):
 
   def tearDown(self): #테스트 케이스 수행 후 
     self.ebest.logout()
+  #C:\stock-lab>workon stocklab
+  #C:\stock-lab>python -m unittest tests.test_agent_ebest
+  #로 단위 테스트 진행이 가능합니다. 
+  def test_code_list(self):
+    print(inspect.stack()[0][3])
+    all_result = self.ebest.get_code_list("ALL")
+    assert all_result is not None
+    kosdaq_result = self.ebest.get_code_list("KOSDAQ")
+    assert kosdaq_result is not None
+    kospi_result = self.ebest.get_code_list("KOSPI")
+    assert kospi_result is not None
+    try:
+      error_result = self.ebest.get_code_list("KOS")
+    except:
+      error_result = None
+    assert error_result is None
+    print("result:", len(all_result),len(kosdaq_result),len(kospi_result))
